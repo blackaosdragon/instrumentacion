c0a66298ae60ac89ffd1fb524ec93bfa2209a082
VictoryTheDragon <ka_ivj@live.com>
2019-11-20 16:49:13 -0600

    correciones de que no compilaba corregidas

--- src/components/contacto.js
+++ src/components/contacto.js
@@ -1,46 +1,45 @@
 import React, {Component} from "react";
 import {Card, CardContent,ListItem,Avatar,ListItemAvatar,ListItemText} from "@material-ui/core"
-import {Call,Mail} from "@material-ui/icons"
+import Call from "@material-ui/icons/Call"
+import Mail from "@material-ui/icons/Mail"
 
 class Contacto extends Component{
     render(){
         return(
             <div>
-                <Card
-          style={{ margin: "20px", marginLeft: "500px", marginRight: "500px" }}
-        >
-          <div class="row center-xs">
-            <CardContent>
-              <div>
-                <ListItem button>
-                  <ListItemAvatar>
-                    <Avatar>
-                      <Call />
-                    </Avatar>
-                  </ListItemAvatar>
-                  <ListItemText
-                    primary="Teléfono oficina"
-                    secondary="(55) 55-83-91-82"
-                  />
-                </ListItem>
-              </div>
+                <Card className="contacto">
+                  <div class="row center-xs">
+                  <CardContent>
+                  <div>
+                    <ListItem button>
+                      <ListItemAvatar>
+                        <Avatar>
+                          <Call />
+                        </Avatar>
+                      </ListItemAvatar>
+                      <ListItemText
+                        primary="Teléfono oficina"
+                        secondary="(55) 55-83-91-82"
+                      />
+                    </ListItem>
+                  </div>
 
-              <div>
-                <ListItem button>
-                  <ListItemAvatar>
-                    <Avatar>
-                      <Mail />
-                    </Avatar>
-                  </ListItemAvatar>
-                  <ListItemText
-                    primary="Correo electrónico"
-                    secondary="instrumentacion_@hotmail.com"
-                  />
-                </ListItem>
+                  <div>
+                    <ListItem button>
+                      <ListItemAvatar>
+                        <Avatar>
+                          <Mail />
+                        </Avatar>
+                      </ListItemAvatar>
+                      <ListItemText
+                        primary="Correo electrónico"
+                        secondary="instrumentacion_@hotmail.com"
+                      />
+                    </ListItem>
+                  </div>
+                </CardContent>
               </div>
-            </CardContent>
-          </div>
-        </Card>
+            </Card>
         
         <div stye={{ margin: " 100px 0px 100px 0px" }}>.</div>
             </div>
--- src/components/home.js
+++ src/components/home.js
@@ -5,7 +5,7 @@ import { GridList, GridListTile, GridListTileBar } from "@material-ui/core"
 import Lampara from "../images/Lampara.jpg"
 import LamparaLimpiesa from "../images/LamparaLimpiesa.jpg"
 import Reparacion from "../images/Reparacion.jpg"
-import Engranes from "../images/EngranesLimpieza.jpg"
+import Engranes from "../images/engranes.jpg"
 import TarjetaLog from "../images/Tarjeta1.jpeg"
 import TarjetaChec from "../images/tarjeta3.jpeg"
 import lcd from "../images/lcdDisplay.jpeg"
@@ -25,9 +25,9 @@ class Home extends Component{
                 />
                 <GridListTileBar
                   className="letrasHome"
-                  title={<p className="tituloCuadricula"> Lamparas de cirugía </p>}
+                  title={<p className="title"> Lamparas de cirugía </p>}
                   subtitle={
-                    <span className="subtitleCuadricula">Mantenimiento Preventlsivo</span>
+                    <span className="description">Mantenimiento Preventlsivo</span>
                   }
             />
               </GridListTile>
@@ -35,56 +35,56 @@ class Home extends Component{
                 <img src={Lampara} alt="Lamparas" />
                 <GridListTileBar
                   className="letrasHome"
-                  title={<p className="tituloCuadricula"> Lamparas de cirugía </p>}
-                  subtitle={<span className="subtitleCuadricula" >Refacciones</span>}
+                  title={<p className="title"> Lamparas de cirugía </p>}
+                  subtitle={<span className="description" >Refacciones</span>}
                 />
               </GridListTile>
               <GridListTile className="cuadriculaImagen" key="Reparacion.jpg">
                 <img src={Reparacion} alt="Mesas" />
                 <GridListTileBar
                   className="letrasHome"
-                  title={<p className="tituloCuadricula"> Mesas quirúrgicas </p>}
-                  subtitle={<span className="subtitleCuadricula">Mantenimiento preventivo</span>}
+                  title={<p className="title"> Mesas quirúrgicas </p>}
+                  subtitle={<span className="description">Mantenimiento preventivo</span>}
                 />
               </GridListTile>
               <GridListTile className="cuadriculaImagen" key="Engranes.jpg">
                 <img src={Engranes} alt="Mesas" />
                 <GridListTileBar
                   className="letrasHome"
-                  title={<p className="tituloCuadricula">Mesas quirúrgicas</p>}
-                  subtitle={<span className="subtitleCuadricula">Servicio correctivo</span>}
+                  title={<p className="title">Mesas quirúrgicas</p>}
+                  subtitle={<span className="description">Servicio correctivo</span>}
                 />
               </GridListTile>
               <GridListTile className="cuadriculaImagen" key="TarjetaLog.jpg">
                 <img src={TarjetaLog} alt="Tarjetas" />
                 <GridListTileBar
                   className="letrasHome"
-                  title={<p className="tituloCuadricula">Tarjetas lógicas</p>}
-                  subtitle={<span className="subtitleCuadricula">Limpieza y mantemiento</span>}
+                  title={<p className="title">Tarjetas lógicas</p>}
+                  subtitle={<span className="description">Limpieza y mantemiento</span>}
                 />
               </GridListTile>
               <GridListTile className="cuadriculaImagen" key="TarjetaChec.jpg">
                 <img src={TarjetaChec} alt="Tarjetas" />
                 <GridListTileBar
                   className="letrasHome"
-                  title={<p className="tituloCuadricula">Tarjetas lógicas</p>}
-                  subtitle={<span className="subtitleCuadricula">Correcto funcionamiento de equipos</span>}
+                  title={<p className="title">Tarjetas lógicas</p>}
+                  subtitle={<span className="description">Correcto funcionamiento de equipos</span>}
                 />
               </GridListTile>
               <GridListTile className="cuadriculaImagen" key="Autoclave.jpg">
                 <img src={Autoclave} alt="Autoclaves" />
                 <GridListTileBar
                   className="letrasHome"
-                  title={<p className="tituloCuadricula">Autoclaves</p>}
-                  subtitle={<span className="subtitleCuadricula">Instalación y mantenimiento a equipos</span>}
+                  title={<p className="title">Autoclaves</p>}
+                  subtitle={<span className="description">Instalación y mantenimiento a equipos</span>}
                 />
               </GridListTile>
               <GridListTile className="cuadriculaImagen" key="lcd.jpg">
                 <img src={lcd} alt="Autoclaves" />
                 <GridListTileBar
                   className="letrasHome"
-                  title={<p className="tituloCuadricula">Autoclaves</p>}
-                  subtitle={<span className="subtitleCuadricula">Uso de microcontroladores y sensores programables</span>}
+                  title={<p className="title">Autoclaves</p>}
+                  subtitle={<span className="description">Uso de microcontroladores y sensores programables</span>}
                 />
               </GridListTile>
 
