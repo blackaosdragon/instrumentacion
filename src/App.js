7053a4a3c30a4c011b722f608d1ee6b565256161
VictoryTheDragon <ka_ivj@live.com>
2020-02-05 13:08:53 -0600

    cascada de push

--- src/App.js
+++ src/App.js
@@ -8,25 +8,53 @@ import MainBar from "./components/mainBar.js"
 import Home from "./components/home.js"
 import Empresa from "./components/empresa"
 import Contacto from "./components/contacto"
+import Footer from "./components/footer.js"
+import Lamparas from "./components/lamparas.js";
+import Mesas from "./components/mesas.js";
+import Aires from "./components/aires.js";
+
+import Sensor from './components/sensor.js';
+
 
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      width: window.innerWidth
+    }
+    this.handleListener = this.handleListener.bind(this);
+  }
+  
+  handleListener(){
+    this.setState({width: window.innerWidth})
+  }
+  componentDidMount(){
+    window.addEventListener("resize", this.handleListener)
+  }
   render() {
+    
     return (
       <div className="App">
         <Router>
         <div >
           <div >.</div>
           <div>
-            <MainBar className="MainBar" />
+            <MainBar className="MainBar" anchura={this.state.width}/>
           </div>
           <Switch>
           
-          <Route exact path="/" component={Home} />
-          <Route path="/empresa" component={Empresa} />
-          <Route path="/contacto" component={Contacto} />
+          <Route exact path="/" component={()=> <Home anchura={this.state.width} />} />
+          <Route path="/empresa" component={() => <Empresa anchura={this.state.width} />} />
+          <Route path="/contacto" component={() => <Contacto anchura={this.state.width} />} />
+          <Route path="/lamparas" component={()=> <Lamparas anchura={this.state.width} />} />
+          <Route path="/mesas" component={()=> <Mesas anchura={this.state.width} />} />
+          <Route path="/aires" component={() => <Aires anchura={this.state.width} />} />
+          <Route path="/sensor" component={() => <Sensor anchura={this.state.width} />} />
+
                     
           </Switch>
+          <Footer anchura={this.state.width} />
         </div>
       </Router>
       </div>
--- src/components/empresa.js
+++ src/components/empresa.js
@@ -1,12 +1,28 @@
 import React, {Component} from "react"
+//import KLSMartinArm from "../images/klsMartin.jpeg"
+//import KLSMartinDes from "../images/KLSMartinDes.jpeg"
+
 
 class Empresa extends Component{
     render(){
-        return(
-            <div>
-                <h1>Empresa</h1>
-            </div>
-        )
+        if (this.props.anchura){
+            return(
+                <div>
+                    <h1>Empresa</h1>
+                    <p>En Instrumentación y Electromecánica nos dedicamos al mantenimiento preventivo y reparación lamparas quirúrgicas Felmex, Dewimed, KLSMartin entre otras marcas</p>
+                    <p>Se realiza intalación de autoclaves</p>
+                </div>
+            )
+
+        } else {
+            return(
+                <div>
+                
+            </div> 
+            )
+                       
+        }
+        
     }
 }
 export default Empresa
